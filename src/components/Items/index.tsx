2efeaa1bcc776a3e6d49616d8d25e4b0806392e8
rodrigoge <rgestevao@gmail.com>
2022-05-11 20:26:27 -0300

    add styles in items and card

--- src/components/Items/index.tsx
+++ src/components/Items/index.tsx
@@ -1,14 +1,55 @@
 import './styles.scss'
 
+import { FiUser, FiMessageSquare, FiSettings, FiHelpCircle } from 'react-icons/fi'
+import { RiNotification3Line } from 'react-icons/ri'
+import { CgLogOut } from 'react-icons/cg'
+//import { Link } from 'react-router-dom';
+
 export default function Items() {
     return (
         <ul>
-            <li><a href="">Meu Perfil</a></li>
-            <li><a href="">Mensagens</a></li>
-            <li><a href="">Notificações</a></li>
-            <li><a href="">Configurações</a></li>
-            <li><a href="">Ajuda</a></li>
-            <li><a href="">Sair</a></li>
+            <li>
+                <a href="">
+                    <FiUser className='icon' />
+                    <span>Meu Perfil</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="">
+                    <FiMessageSquare className='icon' />
+                    <span>Mensagens</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="">
+                    <RiNotification3Line className='icon' />
+                    <span>Notificações</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="">
+                    <FiSettings className='icon' />
+                    <span>Configurações</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="">
+                    <FiHelpCircle className='icon' />
+                    <span>Ajuda</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="">
+                    <CgLogOut className='icon' />
+                    <span>Sair</span>
+                </a>
+            </li>
+
         </ul>
     );
 }
